9fdaefb037156d982d711f5ed304a4203cde6214
Frantzdy Romain <Frantzdyromain@gmail.com>
2016-09-14 15:14:23 -0400

    Update FirstView.cs
    UI benchmarking changes.

--- OxyPlotSampleProject/NewsReader.android/Views/FirstView.cs
+++ OxyPlotSampleProject/NewsReader.android/Views/FirstView.cs
@@ -1,5 +1,6 @@
 using Android.App;
 using Android.OS;
+using Android.Graphics;
 using MvvmCross.Droid.Views;
 
 using NewsReader.Model;
@@ -11,10 +12,15 @@ using OxyPlot.Series;
 using NewsReader.ViewModels;
 using Android.Views;
 using MvvmCross.Binding.Droid.BindingContext;
+using Java.Util;
+using System;
+using Java.Lang;
+using System.Threading.Tasks;
 
 namespace NewsReader.android.Views
 {
     [Activity(Label = "News Reader- OXYPLOT")]
+     
     public class FirstView : MvxActivity
     {
 		//TODO: FIx UI bug where background is black and doesnt show the XY Coordinates
@@ -25,13 +31,33 @@ namespace NewsReader.android.Views
 			set { base.ViewModel = value; }
 		}
 
-		protected override void OnViewModelSet()
-		{
-			base.OnViewModelSet();
-			SetContentView(Resource.Layout.FirstViewOp);
+        protected override void OnViewModelSet()
+        {
+            base.OnViewModelSet();
+            SetContentView(Resource.Layout.FirstViewOp);
+            PlotView pv = this.FindViewById<PlotView>(Resource.Id.plotview);
+            pv.SetBackgroundColor(Color.White);
+             PollPlotModel();
+            //set timer task
+            
+        }
 
-		}
+        private async void PollPlotModel()
+        {
+            while (true)
+            {
+                 ViewModel.UpdatePlotModel();
+                // Update the UI (because of async/await magic, this is still in the UI thread!)
+                if (true)
+                {
+                    Android.Util.Log.Debug("Polling UI", "Polling UI");
+                    await Task.Delay(TimeSpan.FromMilliseconds(1));
+                    //await Task.Delay(TimeSpan.FromTicks(5000));
+                }
+            }
+        }
 
 
     }
+      
 }
