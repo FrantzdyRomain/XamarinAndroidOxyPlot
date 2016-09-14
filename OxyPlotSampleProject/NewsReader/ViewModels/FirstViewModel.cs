3289714555a60ca906344256c028f8fa0ea56720
Frantzdy Romain <Frantzdyromain@gmail.com>
2016-09-14 15:08:47 -0400

    Update FirstViewModel.cs
    Update PlotModel periodically with new data.

--- OxyPlotSampleProject/NewsReader/ViewModels/FirstViewModel.cs
+++ OxyPlotSampleProject/NewsReader/ViewModels/FirstViewModel.cs
@@ -1,22 +1,54 @@
-using MvvmCross.Core.ViewModels;
+ using MvvmCross.Core.ViewModels;
 using NewsReader.Model;
 using OxyPlot;
 using OxyPlot.Axes;
 using OxyPlot.Series;
-
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
 
 namespace NewsReader.ViewModels
 {
     public class FirstViewModel 
         : MvxViewModel
     {
-		 
-        
-		 
 
-		public PlotModel PlotModel
+        public FirstViewModel()
+        {
+            //update model every x interval
+            CreatePlotModel();
+
+        }
+        //some properties and Commands
+        private PlotModel plotModel;
+        private IMvxCommand _listViewBtnClicked;
+        public IMvxCommand ListViewBtnClicked
+        {
+            get
+            {
+                _listViewBtnClicked = _listViewBtnClicked ?? new MvxCommand(btnClick);
+
+                return _listViewBtnClicked;
+            }
+
+        }
+
+
+        private void btnClick()
+        {
+            //Do Something    
+        }
+
+
+
+        public PlotModel PlotModel
 		{
-			get { return CreatePlotModel(); }
+			get { return plotModel; }
+            set {
+                plotModel = value;
+                RaisePropertyChanged(() => PlotModel);
+            }
+
 
 		}
 
@@ -24,17 +56,23 @@ namespace NewsReader.ViewModels
 		//create oxyplot model from properties
 		private PlotModel CreatePlotModel()
 		{
-			var plotModel = new PlotModel { Title = "OxyPlot Demo" };
+            
+
+            plotModel = new PlotModel { Title = "OxyPlot Demo" };
 
 			plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
 			plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Maximum = 10, Minimum = 0 });
 
-			var series1 = new LineSeries
-			{
-				MarkerType = MarkerType.Circle,
-				MarkerSize = 4,
-				MarkerStroke = OxyColors.White
+            var series1 = new LineSeries
+            {
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 4,
+                MarkerStroke = OxyColors.White
+                 
 			};
+            //line series background
+            //series1.Background = Color.White
+
 
 			series1.Points.Add(new DataPoint(0.0, 6.0));
 			series1.Points.Add(new DataPoint(1.4, 2.1));
@@ -48,5 +86,45 @@ namespace NewsReader.ViewModels
 
 			return plotModel;
 		}
+         
+        public async void UpdatePlotModel()
+        {
+            Debug.WriteLine("UpdatePlotModel");
+             
+            var series1 = new LineSeries
+            {
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 4,
+                MarkerStroke = OxyColors.White
+
+            };
+            //line series background
+            //series1.Background = Color.White
+
+            //
+            plotModel.Series.Clear();
+            
+            series1.Points.Add(new DataPoint(randDouble(), randDouble()));
+
+            series1.Points.Add(new DataPoint(randDouble(), randDouble()));
+            series1.Points.Add(new DataPoint(randDouble(), randDouble()));
+            series1.Points.Add(new DataPoint(randDouble(), randDouble()));
+            series1.Points.Add(new DataPoint(randDouble(), randDouble()));
+            series1.Points.Add(new DataPoint(randDouble(), randDouble()));
+            series1.Points.Add(new DataPoint(randDouble(), randDouble()));
+            plotModel.InvalidatePlot(true);
+            plotModel.Series.Add(series1);
+
+            //return plotModel;
+        }
+
+        //create random doubles
+        private Double randDouble() {
+            Double dbl;
+            Random rnd = new Random();
+            int ranInt = rnd.Next(1, 13);
+            dbl = ranInt;
+            return dbl;
+        }
     }
 }
